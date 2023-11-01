61b7d7348a8884ab2a5a6c1ec5e57d8892beefac
Anubis <anubis356@outlook.com>
2023-11-01 18:43:17 +0200

    img directory maker

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,9 +1,11 @@
 import multer from "multer";
 import BadRequest from "../errors/badRequest";
+import { mkdir } from "fs/promises";
 
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    cb(null, "/public/images");
+  destination: async (_, __, cb) => {
+    await mkdir("public/images", { recursive: true });
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
     const fileName = `user-${req.user.id}.${file.mimetype.split("/")[1]}`;
